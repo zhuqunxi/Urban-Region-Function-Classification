e9052089d9c5bbe0365440e2bf78ed45d6e18a31
zhuqunxi <624398575@qq.com>
2019-07-31 15:46:38 -0400

    ..

--- Feature_process/UserID_feature_global/norma_globall.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from Config import config
-from function import main
-
-num = 1
-flag = config.True_Small # config.True_Small  # True: 只测试小数据
-main(num, flag=flag)
---
+++ Feature_process/UserID_feature_global/readme.md
@@ -0,0 +1,6 @@
+## 步骤（提取 global 特征）
+- step 1: 跑main.py, 得到第1个feature
+- step 2: 跑main_more.py, 得到第2个feature
+- step 3: 跑main_more_more.py, 得到第3个feature
+
+
